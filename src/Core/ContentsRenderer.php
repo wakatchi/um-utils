037adf29c033eae797a78e5c3a4791f99447c4a2
wakalab-kurokawa <noriyuki.kurokawa@wakalab.jp>
2024-01-30 10:50:12 +0900

    Change Class Name

--- src/Core/ContentsRenderer.php
+++ src/Core/ContentsRenderer.php
@@ -2,24 +2,24 @@
 
 namespace Wakatchi\WPUtils\Core;
 
-use function Wakatchi\WPUtils\wk_load_content;
+use Wakatchi\WPUtils\WPFunctions;
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
  * Abstract class for rendering contents.
  */
-abstract class Contents_Renderer {
+abstract class ContentsRenderer {
 
     /**
      * Renders a template with the given parameters.
      *
      * @param string $template_name The name of the template to render.
-     * @param array  $template_param Optional. The parameters to pass to the template. Default is an empty array.
+     * @param array $template_param An optional array of parameters to pass to the template.
      * @return string The rendered template.
      */
     protected function render_template( $template_name, $template_param = []){
-        $output = wk_load_content($template_name,$template_param);
+        $output = WPFunctions::load_content($template_name,$template_param);
         return htmlspecialchars_decode(do_shortcode($output), ENT_NOQUOTES) ;
     }
 }
